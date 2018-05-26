5f06ea83adef2b2dbbd353e0fb3bd4d5b07bc912
daya-pangestu <daya.aeist@gmail.com>
2018-05-26 21:32:11 +0700

    perubahan pertama

---
+++ app/src/main/java/com/example/daya/myapplication/DataHelper.java
@@ -0,0 +1,30 @@
+package com.example.daya.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DataHelper(Context context){
+       super(context, DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table biodata (no integer primary key,nama text null,tgl text null,jk text null, alamat text null);";
+        Log.d("data","onCreate"+sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO biodata(no, nama, tgl, jk, alamat)VALUES('1','DAYA','1997-20-3','laki-laki','banjarnegara');";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0,int arg1, int arg2) {
+
+    }
+}
